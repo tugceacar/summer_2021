bc6e7959d667295775c5342d6eb0aeae42fceacd
apple <apple@apples-imac-6.attlocal.net>
2021-06-21 21:50:48 -0400

    for loop completed

---
+++ src/day11loops/WhileLoop01.java
@@ -0,0 +1,29 @@
+package day11loops;
+
+public class WhileLoop01 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 	Print "Hello!" 5 times on the console
+		*/
+		
+		for(int i=1; i<6; i++) {
+			System.out.println("Hello!");
+		}
+		
+		System.out.println("=====================");
+		
+		int i=1;
+		
+		while(i<6) {
+			
+			System.out.println("Hello!");
+			
+			i++;
+			
+		}
+
+	}
+
+}
