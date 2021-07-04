ba3febdc8a4810800958f54a82279eaf58e343e7
apple <apple@apples-imac-6.attlocal.net>
2021-07-04 07:56:41 -0400

    Arrays- multidimensional arrays

---
+++ src/day18arraysmultidimensionalarrays/MultiDimensionalArrays03.java
@@ -0,0 +1,34 @@
+package day18arraysmultidimensionalarrays;
+
+import java.util.Arrays;
+
+public class MultiDimensionalArrays03 {
+
+	public static void main(String[] args) {
+		
+		int arr[][] = { {1,2}, {3,4,5}};
+		
+		int sum = 0;
+		
+		for(int i=0; i<arr.length; i++) {
+			sum = sum + arr[i].length;
+		}
+		
+		int brr[] = new int[sum];
+		int idx = 0;
+		
+		for(int i=0; i<arr.length; i++) {
+			
+			for(int k=0; k<arr[i].length; k++) {
+				
+				brr[idx] = arr[i][k];
+				idx++;
+				
+			}
+		}
+		
+		System.out.println(Arrays.toString(brr));
+
+	}
+
+}
