5e55bbe325533d925f4dc803263321d9a64e1d22
apple <apple@apples-imac-6.attlocal.net>
2021-07-10 16:19:34 -0400

    pass by value

--- src/day18arraysmultidimensionalarrays/MultiDimensionalArrays03.java
+++ src/day18arraysmultidimensionalarrays/MultiDimensionalArrays03.java
@@ -6,14 +6,16 @@ public class MultiDimensionalArrays03 {
 
 	public static void main(String[] args) {
 		
-		int arr[][] = { {1,2}, {3,4,5}};
+		//How to convert 2 dimensional array to 1 dimensional array
+		
+		int arr[][] = { {1,2}, {3,4,5}, {6, 7, 9, 10} };
 		
 		int sum = 0;
 		
 		for(int i=0; i<arr.length; i++) {
 			sum = sum + arr[i].length;
 		}
-		
+
 		int brr[] = new int[sum];
 		int idx = 0;
 		
