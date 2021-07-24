d5fb0d8ecd0e06a922c4d57d62663f953aa7f3b7
apple <apple@apples-imac-6.attlocal.net>
2021-07-24 12:24:19 -0400

    super ended

--- src/day26inheritanceoverriding/Bird.java
+++ src/day26inheritanceoverriding/Bird.java
@@ -1,4 +1,4 @@
-package day26inheritance;
+package day26inheritanceoverriding;
 
 /*
  	super()
@@ -11,9 +11,38 @@ package day26inheritance;
 
 public class Bird extends Mammal{
 	
+	String name = "Bird";
+	int height = 11;
+	byte ageOfBird = 1;
+	
 	public Bird() {
 		super(3);
+		System.out.println(super.ageOfAnimal);
+		System.out.println(super.ageOfMammal);
+		System.out.println(super.height);
+		System.out.println(super.name);
+		
+		super.move();//Mammals move
+		
 		System.out.println("Child constructor ran...");
 	}
-
+	
+	public void move() {
+		System.out.println("Birds move...");
+	}
+	
+	public void fly() {
+		System.out.println("Birds fly...");
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/day26inheritanceoverriding/Mammal.java
@@ -0,0 +1,22 @@
+package day26inheritanceoverriding;
+
+public class Mammal extends Animal{
+	
+	String name = "Mammal";
+	int height = 22;
+	byte ageOfMammal = 2;
+	
+	public Mammal(int height) {
+		super();
+		System.out.println(height + " cm.");
+	}
+	
+	public void move() {
+		System.out.println("Mammals move...");
+	}
+	
+	public void feed() {
+		System.out.println("Mammals feed their babies with milk...");
+	}
+
+}
