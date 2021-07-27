835d76aa5e4eb0c166a5be1f2a7fe51a9e643be7
apple <apple@apples-imac-6.attlocal.net>
2021-07-26 21:56:36 -0400

    Exceptions

---
+++ src/day27exceptions/E03.java
@@ -0,0 +1,24 @@
+package day27exceptions;
+
+public class E03 {
+
+	public static void main(String[] args) {
+		
+		int a  = 12;
+		int b = 3;
+		
+		try {
+			
+			System.out.println(a / b);
+			
+		}catch(ArithmeticException e) {
+			
+			System.out.println("Do not use zero as divisor...");
+
+			
+		}
+		
+
+	}
+
+}
